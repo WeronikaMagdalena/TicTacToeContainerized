f2703ead9f2d25e45620c0ac8441373a68008ce2
WeronikaMagdalena <alu0101696997@ull.edu.es>
2024-05-03 00:55:57 +0100

    game over screen ok

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,7 @@
 const tiles = document.querySelectorAll(".tile");
+const gameOverArea = document.getElementById("game-over-area");
+const gameOverText = document.getElementById("game-over-text");
+const playAgain = document.getElementById("play-again");
 
 tiles.forEach((tile) => tile.addEventListener("click", function () {
   const tileNumber = tile.dataset.index;
@@ -8,8 +11,16 @@ tiles.forEach((tile) => tile.addEventListener("click", function () {
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data.board);
+      console.log(data);
       updateBoard(data.board);
+      if (data.gameOver == true) {
+        gameOverArea.className = "visible";
+        let text = "Draw!";
+        if (data.winner) {
+          text = `Winner is ${data.winner}!`;
+        }
+        gameOverText.innerText = text;
+      }
     })
     .catch(error => console.error('Error:', error));
 }));
@@ -18,4 +29,17 @@ function updateBoard(boardState) {
   tiles.forEach((tile, index) => {
     tile.innerText = boardState[index];
   });
+}
+
+playAgain.addEventListener("click", function () {
+  fetch('http://localhost:8080/game/start', {
+    method: 'POST'
+  })
+  console.log("New game started!");
+  gameOverArea.className = "hidden";
+  clearBoard();
+});
+
+function clearBoard() {
+  tiles.forEach((tile) => (tile.innerText = ""));
 }
